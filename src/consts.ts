907462878746f61a73020255abfbcbdc64c43d8c
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2023-11-06 22:09:26 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Mentiras Políticas';
+export const SITE_DESCRIPTION = 'Recopilación de Mentiras de nuestros políticos, para algunos denominado: Cambios de opinión';
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const mentira = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		lieDate: z.coerce.date(),
+		author: z.string().optional(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		featured: z.boolean().optional(),
+		draft: z.boolean().optional(),
+		tags: z.array(z.string()).default(["others"]),
+	})
+});
+
+export const collections = { mentira };
